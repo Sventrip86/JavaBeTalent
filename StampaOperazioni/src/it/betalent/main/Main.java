a120d74dfa3567db3df27aeb315b7f4038c4cf99
Sventrip86 <fab.sebis@gmail.com>
2023-06-20 14:12:01 +0200

    added StampaOperazioni

---
+++ StampaOperazioni/src/it/betalent/main/Main.java
@@ -0,0 +1,15 @@
+package it.betalent.main;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		int numero1 = 10;
+		int numero2 = 3;
+		
+		System.out.println("La somma di " + numero1 + " + " + numero2 + " e' : " + (numero1+numero2));
+		System.out.println("Il modulo di " + numero1 + " % " + numero2 + " e' : " + (numero1%numero2));
+		
+
+	}
+
+}
---
+++ StampaTipi/src/it/betalent/main/Main.java
@@ -0,0 +1,17 @@
+package it.betalent.main;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		int numero = 36; //primitivo 
+		String stringa = "Questa e' una stringa di tipo reference!"; // reference
+		Integer numeroIntero = 40; // wrapper 
+		
+		
+		System.out.println("Tipo di dato primitivo: " + numero);
+		System.out.println("Tipo di dato reference: " + stringa);
+		System.out.println("Tipo di dato wrapper: " + numeroIntero);
+
+	}
+
+}
